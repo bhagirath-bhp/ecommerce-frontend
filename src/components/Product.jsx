5bb029d8f4755911217e8c80a85d50f68b884a3c
bhagirath-bhp <bhagirath.bhp@gmail.com>
2023-12-30 21:06:50 +0530

    Limited Quantities tag added

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,18 +2,18 @@ import { IoMdArrowDroprightCircle } from "react-icons/io";
 
 const Product = (props) => {
     return (
-        // Add hover effects for shadow and scale for a subtle animation
         <div className="product min-w-[350px] overflow-hidden rounded-lg border-[1px] border-black m-5 transition duration-300 ease-in-out transform hover:shadow-lg hover:scale-105 cursor-pointer">
             <div className="product-img w-full h-[261px] overflow-hidden">
-                <img src={props.url} alt={props.title} className='w-full h-full object-cover' /> {/* Added h-full object-cover for better image fit */}
+                <img src={props.url} alt={props.title} className='w-full h-full object-cover' />
             </div>
             <div className="product-details px-5 pt-5">
                 <h3 className="product-title font-bold text-3xl">{props.title}</h3>
                 <p className="product-description">{props.description}</p>
+                <p className="my-5 border-[1px] rounded-xl font-normal text-base text-center px-5 bg-goldenLight text-golden border-golden">Limited Quantities Left</p>
             </div>
             <div className="product-price flex justify-between items-center p-5">
                 <h3 className="product-title font-bold text-3xl">₹{props.price}</h3>
-                <IoMdArrowDroprightCircle className='icon text-black cursor-pointer' /> {/* Added cursor-pointer for icon interaction */}
+                <IoMdArrowDroprightCircle className='icon text-black cursor-pointer' />
             </div>
         </div>
     )
