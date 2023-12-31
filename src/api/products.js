d3744723616910d5dd1027075a99124721c523c9
bhagirath-bhp <bhagirath.bhp@gmail.com>
2023-12-31 22:54:26 +0530

    API Integration: product, cart, wishlist

--- src/api/products.js
+++ src/api/products.js
@@ -39,3 +39,204 @@ export const fetchCollections = async() => {
     }
     else return null
 }
+
+// Add a new product 
+export const addNewProduct = async (productData) => {
+    try {
+        const response = await axios.post(`${uri}/product/add`, productData, {
+            headers: {
+                'Authorization': `Bearer ${Cookies.get('token')}`
+            }
+        });
+
+        if (response.status === 200) {
+            return response.data;
+        } else {
+            return null;
+        }
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+};
+
+
+// Add a new category
+export const addNewCategory = async (categoryName) => {
+    try {
+        const response = await axios.post(`${uri}/categories`, { categoryName }, {
+            headers: {
+                'Authorization': `Bearer ${Cookies.get('token')}`
+            }
+        });
+
+        if (response.status === 200) {
+            return response.data;
+        } else {
+            return null;
+        }
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+};
+
+// Get all categories
+export const getAllCategories = async () => {
+    try {
+        const response = await axios.get(`${uri}/categories`, {
+            headers: {
+                'Authorization': `Bearer ${Cookies.get('token')}`
+            }
+        });
+
+        if (response.status === 200) {
+            return response.data;
+        } else {
+            return null;
+        }
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+};
+
+// Add a new collection
+export const addNewCollection = async (collectionName) => {
+    try {
+        const response = await axios.post(`${uri}/collections`, { name: collectionName }, {
+            headers: {
+                'Authorization': `Bearer ${Cookies.get('token')}`
+            }
+        });
+
+        if (response.status === 200) {
+            return response.data;
+        } else {
+            return null;
+        }
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+};
+
+// Get all collections
+export const getAllCollections = async () => {
+    try {
+        const response = await axios.get(`${uri}/collections`, {
+            headers: {
+                'Authorization': `Bearer ${Cookies.get('token')}`
+            }
+        });
+
+        if (response.status === 200) {
+            return response.data;
+        } else {
+            return null;
+        }
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+};
+
+// Get all products
+export const getAllProducts = async () => {
+    try {
+        const response = await axios.get(`${uri}/products`, {
+            headers: {
+                'Authorization': `Bearer ${Cookies.get('token')}`
+            }
+        });
+
+        if (response.status === 200) {
+            return response.data;
+        } else {
+            return null;
+        }
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+};
+
+// Get a single product by ID
+export const getAProductById = async (productId) => {
+    try {
+        const response = await axios.get(`${uri}/products/${productId}`, {
+            headers: {
+                'Authorization': `Bearer ${Cookies.get('token')}`
+            }
+        });
+
+        if (response.status === 200) {
+            return response.data;
+        } else {
+            return null;
+        }
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+};
+
+// Update a product
+export const updateProduct = async (productId, updatedData) => {
+    try {
+        const response = await axios.put(`${uri}/products/${productId}`, updatedData, {
+            headers: {
+                'Authorization': `Bearer ${Cookies.get('token')}`
+            }
+        });
+
+        if (response.status === 200) {
+            return response.data;
+        } else {
+            return null;
+        }
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+};
+
+// Delete a product
+export const deleteProductById = async (productId) => {
+    try {
+        const response = await axios.delete(`${uri}/products/${productId}`, {
+            headers: {
+                'Authorization': `Bearer ${Cookies.get('token')}`
+            }
+        });
+
+        if (response.status === 200) {
+            return response.data;
+        } else {
+            return null;
+        }
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+};
+
+// Get products by category ID
+export const getProductsByCategoryId = async (categoryId) => {
+    try {
+        const response = await axios.get(`${uri}/categories/${categoryId}/products`, {
+            headers: {
+                'Authorization': `Bearer ${Cookies.get('token')}`
+            }
+        });
+
+        if (response.status === 200) {
+            return response.data;
+        } else {
+            return null;
+        }
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+};
