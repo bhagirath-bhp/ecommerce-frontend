166a42431413994ab79d09df523322f16cf54568
bhagirath-bhp <bhagirath.bhp@gmail.com>
2024-02-19 22:06:15 +0530

    Fixed review component

--- src/components/ReviewBox.jsx
+++ src/components/ReviewBox.jsx
@@ -1,20 +1,33 @@
 import { Progress, Rating } from "@material-tailwind/react";
 import TestimonialCarousel from "./TestimonialCarousel";
+import { getRatingsForAProduct } from "../api/reviews";
+import { useEffect, useState } from "react";
+
 
 const ReviewBox = ({productId}) => {
+    const [rating, setRating] = useState(0);
+    const [totalReviews, setTotalReviews] = useState(0);
+    useEffect(()=>{
+        const fetchRatings = async () => {
+            const response = await getRatingsForAProduct(productId);
+            setRating(response.avgRating);
+            setTotalReviews(response.ratings.length);
+        }
+        fetchRatings();
+    }, [])
     return (
         <div className="grid grid-rows-1 grid-cols-3">
             <div className="review-stats py-5">
                 <div className="rating-head row-start-1 row-end-1 col-span-1">
-                    <Rating value={4} readonly/>
+                    <Rating value={rating} readonly/>
                     <p className="font-medium text-blue-gray-500">
-                        4 out of 5
+                        {rating} out of 5
                     </p>
                     <p className="font-medium text-blue-gray-500">
-                        Based on 134 Reviews
+                        Based on {totalReviews} Reviews
                     </p>
                 </div>
-                <div className="review-bars smMobile:w-[90vw] tablet:w-full tablet:col-start-1 tablet:col-end-2 smMobile:col-span-4">
+                {/* <div className="review-bars smMobile:w-[90vw] tablet:w-full tablet:col-start-1 tablet:col-end-2 smMobile:col-span-4">
                     <div className="review-star-box flex">
                         <p className="review-star">5 star</p>
                         <Progress className="progress-bar" value={60} color="yellow"/>
@@ -40,7 +53,7 @@ const ReviewBox = ({productId}) => {
                         <Progress className="progress-bar" value={60} color="yellow"/>
                         <p className="review-percent">60 %</p>
                     </div>
-                </div>
+                </div> */}
             </div>
             <div className="review-cards tablet:col-start-2 tablet:col-end-5 smMobile:col-span-4 overflow-hidden">
                 <TestimonialCarousel type="review" productId={productId}/>
